4ae0500dc100da2b800a2711ffe70cda601e8d52
Andrew <yueh.andrew@gmail.com>
2020-04-05 15:47:25 -0700

    added type validators script

--- docUtils/makeAvailableValidators.ts
+++ docUtils/makeAvailableValidators.ts
@@ -1,9 +1,17 @@
-(function() {
-    const typeMatchers = require('../src/typeMatchers')
+import typeMatchers from '../src/typeMatchers'
+import fs from 'fs'
 
+(function () {
+    console.log(typeMatchers)
     const keys = [...typeMatchers].map(([el]) => {
-        console.log(el.name || 'hhello')
+        return el && el.name ? `- \`${el.name}\` (Constructor)` : `- \`${el}\``
     })
-    console.log('????')
-    console.log(keys)
+    const uniqueNames = [...new Set(keys)]
+        .filter(el => typeof el === 'string' ? !el.includes('enforce') : true)
+        .join('\n')
+const availableMatchersText = `#### Available Matchers
+
+##### Description 
+Out-of-the-box type validators. See examples for usage.`
+    fs.writeFileSync(`./testFile.md`, `${availableMatchersText}\n${uniqueNames}\n`)
 })()
