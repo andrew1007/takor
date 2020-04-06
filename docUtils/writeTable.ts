e3030f4908a4eacfae6581d8996e2a96174edd1b
Andrew <yueh.andrew@gmail.com>
2020-04-06 08:09:44 -0700

    finished core logic of doc writing

--- docUtils/writeTable.ts
+++ docUtils/writeTable.ts
@@ -2,24 +2,29 @@
 (async function () {
     const TAKOR_DOCS_DIRECTORY = '../docs/takor'
     const fs = require('fs')
-    let tableHeader = '|static|Description|'
-    let tableSeparator = '|--|--|'
+    let tableHeader = '|static| type| description |'
+    let tableSeparator = '|--| -- | -- |'
     const takorNames: string[] = []
     const converseNames: string[] = []
+    const type: string[] = []
+    const converseType: string[] = []
     const description: string[] = []
     const converseDescription: string[] = []
     await new Promise(resolve => {
         fs.readdir(TAKOR_DOCS_DIRECTORY, (_, files) => {
             for (let file of files) {
                 const text = fs.readFileSync(`${TAKOR_DOCS_DIRECTORY}/${file}`, 'utf8')
-                const name = text.split('\n')[0].replace(/[^a-zA-Z\.]/g, '')
-                const desc = text.split('\n')[3]
-                if (name.includes('not.')) {
-                    converseNames.push(name)
-                    converseDescription.push(desc)
+                const nameText = text.split('\n')[0].replace(/[^a-zA-Z\.]/g, '')
+                const typeText = text.split('\n')[2].replace('type: ', '')
+                const descText = text.split('\n')[5]
+                if (nameText.includes('not.')) {
+                    converseNames.push(nameText)
+                    converseType.push(typeText)
+                    converseDescription.push(descText)
                 } else {
-                    takorNames.push(`${name}`)
-                    description.push(desc)
+                    takorNames.push(`${nameText}`)
+                    type.push(typeText)
+                    description.push(descText)
                 }
             }
             resolve()
@@ -27,10 +32,10 @@
     })
 
     const rows = takorNames.map((name, idx) => {
-        return `|${name}| ${description[idx]} |`
+        return `|${name}| ${type[idx]} | ${description[idx]} |`
     }).join('\n')
     const converseRows = converseNames.map((name, idx) => {
-        return `|${name}| ${converseDescription[idx]} |`
+        return `|${name}| ${converseType[idx]} | ${converseDescription[idx]} |`
     }).join('\n')
     console.log(tableHeader)
     console.log(tableSeparator)
