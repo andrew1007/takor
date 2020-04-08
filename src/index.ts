785aaa30f5524abc7cc2994817d6f1d3bb875eb2
Andrew <yueh.andrew@gmail.com>
2020-04-07 20:39:58 -0700

    added comments

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,35 @@ import TakorUtils from './TakorUtils'
 import { ShapeOfMatchers, IMatcher } from './types'
 
 export default class takor extends TakorUtils {
+    /**
+     * @param any
+     * @description matcher that is intended to be used with an actual validator
+     */
     static pojo = TakorUtils.pojo
+
+    /**
+     * @param any
+     * @description matcher that is intended to be used with an actual validator
+     */
     static any = TakorUtils.ANY
+
+    /**
+     * @param any
+     * @description matcher that is intended to be used with an actual validator
+     */
     static truthy = TakorUtils.TRUTHY
+
+    /**
+     * @param any
+     * @description matcher that is intended to be used with an actual validator
+     */
     static falsey = TakorUtils.FALSEY
 
+    /**
+     * @param shape pojo of one level of nesting
+     * @description Checks keys of pojo. 
+     * @note Nest another takor.shape if you want to assert nested keys
+     */
     static shape(shape: ShapeOfMatchers) {
         let shapeSize: number
         try {
@@ -41,11 +65,19 @@ export default class takor extends TakorUtils {
         }
     }
 
+    /**
+     * @param enforcedTypes arbitrary number of matchers
+     * @description All elements in the Array must meet at least one matcher
+     */
     static arrayOf(...enforcedTypes: IMatcher[]) {
         const allValidators = TakorUtils.createArrayValidators(enforcedTypes)
         return TakorUtils.assertOfArray(allValidators)
     }
 
+    /**
+     * @param enforcedTypes arbitrary number of matchers
+     * @description All elements in the Set must meet at least one matcher
+     */
     static setOf(...enforcedTypes: IMatcher[]) {
         const allValidators = (enforcedTypes).map(el => TakorUtils.getValidator(el))
         return (set: any) => {
@@ -56,6 +88,10 @@ export default class takor extends TakorUtils {
         }
     }
 
+    /**
+     * @param enforcedTypes arbitrary number of tuple matchers for [key, value]
+     * @description All elements in the Map must meet at least one matcher
+     */
     static mapOf(...enforcedTypes: [IMatcher, IMatcher][]) {
         let allValidators
         try {
@@ -76,22 +112,39 @@ export default class takor extends TakorUtils {
         }
     }
 
+    /**
+     * @param enforcedType single matcher
+     * @description Element must meet that one single matcher
+     */
     static is(enforcedType: IMatcher) {
         const validator = TakorUtils.getValidator(enforcedType)
         return (el: any) => validator(el)
     }
 
+    /**
+     * @param enforcedTypes arbitrary number of matchers
+     * @description The element checked must meet at least one of the matchers
+     */
     static oneOf(...enforcedTypes: IMatcher[]) {
         const allValidators = TakorUtils.createArrayValidators(enforcedTypes)
         return TakorUtils.ensure.some(allValidators)
     }
 
+    /**
+     * @param enforcedTypes arbitrary number of matchers
+     * @description The element checked must meet _every_ matcher
+     * @note If the list of matchers contradict each other, it will always return false
+     */
     static allOf(...enforcedTypes: IMatcher[]) {
         const allValidators = TakorUtils.createArrayValidators(enforcedTypes)
         return TakorUtils.ensure.every(allValidators)
     }
 
     static not = {
+        /**
+         * @param enforcedTypes arbitrary number of matchers
+         * @description converse of setOf
+         */
         setOf(...enforcedTypes: IMatcher[]) {
             const allValidators = TakorUtils.createArrayValidators(enforcedTypes)
             return (set: any) => {
@@ -103,6 +156,10 @@ export default class takor extends TakorUtils {
                 })
             }
         },
+        /**
+         * @param enforcedTypes arbitrary number of tuple matchers for [key, value]
+         * @description converse of mapOf
+         */
         mapOf(...enforcedTypes: [IMatcher, IMatcher][]) {
             let allValidators
             try {
@@ -121,14 +178,26 @@ export default class takor extends TakorUtils {
                 })
             }
         },
+        /**
+         * @param enforcedType single matcher
+         * @description converse of is
+         */
         is: (enforcedType: IMatcher) => {
             const validator = TakorUtils.getValidator(enforcedType)
             return (element: any) => !validator(element)
         },
+        /**
+         * @param enforcedTypes arbitrary number of matchers
+         * @description converse of oneOf
+         */
         oneOf: (...enforcedTypes: IMatcher[]) => {
             const allValidators = TakorUtils.createArrayValidators(enforcedTypes)
             return TakorUtils.ensure.not.every(allValidators)
         },
+        /**
+         * @param enforcedTypes arbitrary number of matchers
+         * @description converse of arrayOf
+         */
         arrayOf: (...enforcedTypes: IMatcher[]) => {
             const allValidators = TakorUtils.createArrayValidators(enforcedTypes)
 
@@ -141,6 +210,11 @@ export default class takor extends TakorUtils {
                 })
             }
         },
+        /**
+         * @param shape pojo of one level of nesting
+         * @description converse of shape
+         * @note Nest another takor.shape if you want to assert nested keys
+         */
         shape: (shape: ShapeOfMatchers = {}) => {
             let shapeSize
             try {
